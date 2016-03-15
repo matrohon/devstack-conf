47f3db5c3f42d733bc3684352e2828e0c4de3b4b
Mathieu Rohon <mathieu.rohon@gmail.com>
2016-03-15 14:24:37 +0000

    Adding heat template

--- test_bgpvpn_bagpipe/stack-bgpvpn.sh
+++ test_bgpvpn_bagpipe/stack-bgpvpn.sh
@@ -3,8 +3,9 @@
 sudo ovs-vsctl del-br br-mpls
 sudo ovs-vsctl add-br br-mpls
 
-#sudo service bagpipe-fakerr restart
-#sudo service bagpipe-bgp restart
+./stack.sh
 
-#OFFLINE=True ./stack.sh
-RECLONE=True ./stack.sh
+source openrc admin demo
+
+nova keypair-add heat_key > /tmp/heat_key.priv
+heat stack-create -f stack.yaml -P key_name=heat_key bgpvpn
